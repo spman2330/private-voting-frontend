0f5b9ddd64b0ee918a128729c51753539bac5c3c
spman2330 <vuhoainam4040@gmail.com>
2022-12-23 15:01:01 +0700

    update contract context

--- src/contexts/contract-context/index.js
+++ src/contexts/contract-context/index.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { ADDRESSES } from "./address";
 import token from "./abis/Token.json";
+import voting from "./abis/Voting.json"
 import { useWeb3Context } from "../web3-context";
 import { BigNumber, ethers } from "ethers";
 const ContractContext = createContext({});
@@ -10,17 +11,34 @@ export function ContractProvider({
     const {
         web3Provider,
         rpcProvider,
+        address,
         chain
     } = useWeb3Context();
+
     const tokenCallContract = useMemo(() => {
         return new ethers.Contract(ADDRESSES[chain.chainId].token, token.abi, rpcProvider);
     }, [chain, rpcProvider]);
+
+    const votingCallContract = useMemo(() => {
+        return new ethers.Contract(ADDRESSES[chain.chainId].voting, voting.abi, rpcProvider);
+    }, [chain, rpcProvider]);
+
+    const votingTransContract = useMemo(() => {
+        if (web3Provider) {
+            return new ethers.Contract(ADDRESSES[chain.chainId].voting, voting.abi, web3Provider.getSigner(address));
+        } else {
+            return undefined;
+        }
+    }, [chain, web3Provider, address]);
     const contextData = useMemo(() => {
         return {
-            tokenCallContract
+            tokenCallContract,
+            votingCallContract,
+            votingTransContract
         }
     }, [web3Provider,
         rpcProvider,
+        address,
         chain]);
     return (
         <ContractContext.Provider value={contextData}>
